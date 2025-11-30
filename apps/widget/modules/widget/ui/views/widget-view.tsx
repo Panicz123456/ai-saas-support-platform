612f4cdc6637d4f177db9ecd16cfee72107b0d7a
patryk <paniczgg@gmail.com>
2025-11-30 02:09:28 +0100

    widget Inbox

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -3,14 +3,13 @@
 import { useAtomValue } from 'jotai';
 
 import { screenAtom } from '@/modules/widget/atoms/widget-atoms';
-// import { WidgetFooter } from "@/modules/widget/ui/components/widget-footer"
-// import { WidgetHeader } from "@/modules/widget/ui/components/widget-header"
 
 import { WidgetChatScreen } from '@/modules/widget/ui/screens/widget-chat-screen';
 import { WidgetAuthScreen } from '@/modules/widget/ui/screens/widget-auth-screen';
 import { WidgetErrorScreen } from '@/modules/widget/ui/screens/widget-error-screen';
 import { WidgetLoadingScreen } from '@/modules/widget/ui/screens/widget-loading-screen';
 import { WidgetSelectionScreen } from '@/modules/widget/ui/screens/widget-selection-screen';
+import { WidgetInboxScreen } from '@/modules/widget/ui/screens/widget-inbox-screen';
 
 interface Props {
 	organizationId: string | null;
@@ -25,15 +24,14 @@ export const WidgetView = ({ organizationId }: Props) => {
 		loading: <WidgetLoadingScreen organizationId={organizationId} />,
 		selection: <WidgetSelectionScreen />,
 		chat: <WidgetChatScreen />,
+		inbox: <WidgetInboxScreen />,
 		voice: <p>TODO: voice screen</p>,
-		inbox: <p>TODO: inbox screen</p>,
 		contact: <p>TODO: contact screen</p>,
 	};
 
 	return (
 		<main className=" min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
 			{screenComponents[screen]}
-			{/* <WidgetFooter /> */}
 		</main>
 	);
 };
--- packages/backend/convex/public/conversation.ts
+++ packages/backend/convex/public/conversation.ts
@@ -2,44 +2,98 @@ import { ConvexError, v } from 'convex/values';
 
 import { mutation, query } from '../_generated/server';
 import { supportAgent } from '../system/ai/agents/supportAgent';
-import { saveMessage } from '@convex-dev/agent';
+import { MessageDoc, saveMessage } from '@convex-dev/agent';
 import { components } from '../_generated/api';
+import { paginationOptsValidator } from 'convex/server';
+
+export const getMany = query({
+	args: {
+		contactSessionId: v.id('contactSession'),
+		paginationOpts: paginationOptsValidator,
+	},
+	handler: async (ctx, args) => {
+		const session = await ctx.db.get(args.contactSessionId);
+		if (!session || session.expairedAt < Date.now()) {
+			throw new ConvexError({
+				code: 'UNAUTHORIZED',
+				message: 'Invalid Session',
+			});
+		}
+
+		const conversations = await ctx.db
+			.query('conversation')
+			.withIndex('by_contact_session_id', (q) =>
+				q.eq('contactSessionId', args.contactSessionId)
+			)
+			.order('desc')
+			.paginate(args.paginationOpts);
+
+		const conversationLastMessage = await Promise.all(
+			conversations.page.map(async (conversations) => {
+				let lastMessage: MessageDoc | null = null;
+
+				const messages = await supportAgent.listMessages(ctx, { 
+					threadId: conversations.threadId,
+					paginationOpts: {numItems: 1, cursor: null}
+				})
+
+				if (messages.page.length > 0) { 
+					lastMessage = messages.page[0] ?? null
+				}
+
+				return {
+					_id: conversations._id,
+					creationTime: conversations._creationTime,
+					status: conversations.status,
+					organizationId: conversations.organizationId,
+					threadId: conversations.threadId,
+					lastMessage,
+				};
+			})
+		);
+
+		return { 
+			...conversations,
+			page: conversationLastMessage
+		}
+	},
+});
 
 export const getOne = query({
 	args: {
-		conversationId: v.id("conversation"),
+		conversationId: v.id('conversation'),
 		contactSessionId: v.id('contactSession'),
 	},
 	handler: async (ctx, args) => {
-		const session = await ctx.db.get(args.contactSessionId)
-		if (!session || session.expairedAt < Date.now()) { 
-			throw new ConvexError({ 
-				code: "UNAUTHORIZED",
-				message: "Invalid Session"
-			})
+		const session = await ctx.db.get(args.contactSessionId);
+		if (!session || session.expairedAt < Date.now()) {
+			throw new ConvexError({
+				code: 'UNAUTHORIZED',
+				message: 'Invalid Session',
+			});
 		}
 
 		const conversation = await ctx.db.get(args.conversationId);
 
-		if (!conversation) { 
+		if (!conversation) {
 			throw new ConvexError({
 				code: 'UNAUTHORIZED',
 				message: 'Conversation not found',
 			});
 		}
 
-		if (conversation.contactSessionId !== session._id) { 
+		if (conversation.contactSessionId !== session._id) {
 			throw new ConvexError({
 				code: 'UNAUTHORIZED',
 				message: 'Incorrect Session',
 			});
 		}
 
-		return {  
+		return {
 			_id: conversation._id,
 			status: conversation.status,
-			threadId: conversation.threadId
-		}
+			threadId: conversation.threadId,
+		};
 	},
 });
 
@@ -56,27 +110,27 @@ export const create = mutation({
 				message: 'Invalid session',
 			});
 		}
-		
-		const { threadId } = await supportAgent.createThread(ctx, { 
+
+		const { threadId } = await supportAgent.createThread(ctx, {
 			userId: args.organizationId,
-		})
+		});
 
-		await saveMessage(ctx, components.agent, { 
+		await saveMessage(ctx, components.agent, {
 			threadId,
-			message: { 
-				role: "assistant",
+			message: {
+				role: 'assistant',
 				// TODO: modify to widget setrtings inital message
-				content: "Hello, how can I help you today?"
-			}
-		})
+				content: 'Hello, how can I help you today?',
+			},
+		});
 
-    const conversationId = await ctx.db.insert('conversation', {
+		const conversationId = await ctx.db.insert('conversation', {
 			contactSessionId: session._id,
 			status: 'unresolved',
 			organizationId: args.organizationId,
 			threadId,
 		});
 
-    return conversationId
+		return conversationId;
 	},
 });
