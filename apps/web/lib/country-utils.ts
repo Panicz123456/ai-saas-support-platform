109f36e2b30cd157387a7c0fbceb19f59d96938f
patryk <paniczgg@gmail.com>
2025-12-01 18:35:14 +0100

    dashboard web index

---
+++ apps/web/lib/country-utils.ts
@@ -0,0 +1,20 @@
+import * as city from 'countries-and-timezones'
+
+export function getCountryFromTimezone(timezone?: string) { 
+  if (!timezone) return null
+  
+  const timezoneInfo = city.getTimezone(timezone)
+  if (!timezoneInfo?.countries?.length) return null
+  
+  const countryCode = timezoneInfo.countries[0]
+  const country = city.getCountry(countryCode as string)
+
+  return { 
+    code: countryCode,
+    name: country?.name || countryCode,
+  }
+}
+
+export function getCountrtFlagUrl(countryCode: string) { 
+  return `https://flagcdn.com/w20/${countryCode.toLowerCase()}.png`
+}
